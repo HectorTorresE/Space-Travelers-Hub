0b1f3b5f56e45856379fa14fe61b0ba8afae0d48
HectorTorresE <hectorjte@gmail.com>
2022-04-05 10:34:02 -0600

    Add views and the redirect necesarry in the navbar also add some styling to the Navbar

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
+import NavBar from './components/Navbar';
+import { Route, Routes } from 'react-router-dom';
+import Rockets from './views/Rockets';
+import Missions from './views/Missions';
+import Profile from './views/Profile';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <NavBar/>
+      <Routes>
+        <Route path='/' element={<Rockets/>}/>
+        <Route path='/missions' element={<Missions/>}/>
+        <Route path='/profile' element={<Profile/>}/>
+      </Routes>
     </div>
   );
 }
