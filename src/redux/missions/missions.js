5335cd0eef28933d143ada54e2c30b0aed78acb8
JuanLP <juan.luis.palacios.p@gmail.com>
2022-04-05 14:33:00 -0500

    Add fetch from API to action creator

--- src/redux/missions/missions.js
+++ src/redux/missions/missions.js
@@ -1,4 +1,7 @@
 const LIST = 'space-travelers-hub/missions/LIST';
+const LIST_SUCCESS = 'space-travelers-hub/missions/LIST_SUCCESS';
+const LIST_FAILURE = 'space-travelers-hub/missions/LIST_FAILURE';
+
 const defaultMissions = [];
 
 export default function reducer(state = defaultMissions, action = {}) {
@@ -6,9 +9,23 @@ export default function reducer(state = defaultMissions, action = {}) {
     case LIST: {
       return [];
     }
+    case LIST_SUCCESS: {
+      return [...action.list];
+    }
     default:
       return state;
   }
 }
 
-export const getMissions = () => ({ type: LIST });
+export const getMissions = () => (dispatch) => {
+  dispatch({ type: LIST });
+  return fetch('https://api.spacexdata.com/v3/missions').then(
+    (request) => request.json().then((map) => dispatch({
+      type: LIST_SUCCESS,
+      list: Object.keys(map)
+      // eslint-disable-next-line camelcase
+        .map((item_id) => map[item_id].map((book) => ({ ...book, item_id }))).flat(),
+    })),
+    (err) => dispatch({ type: LIST_FAILURE, err }),
+  );
+};
