486c7576e4546f008dccaacca1d08d20daccce65
JuanLP <juan.luis.palacios.p@gmail.com>
2022-04-06 11:16:47 -0500

    Add Profile basic structure

--- src/views/Profile.js
+++ src/views/Profile.js
@@ -1,5 +1,33 @@
-const Profile = () => (
-  <h1>Profile</h1>
-);
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { getMissions } from '../redux/missions/missions';
+
+const Profile = () => {
+  const dispatch = useDispatch();
+  const missions = useSelector(
+    (store) => (store.missions ? store.missions.filter((x) => x.reserved) : store.missions),
+  );
+
+  useEffect(() => {
+    if (missions === null)dispatch(getMissions());
+  }, [missions]);
+  return (
+    <div className="Profile">
+      <div>
+        <h1>My Missions</h1>
+        <ul>
+          {(missions || []).map((mission) => (
+            <li key={mission.mission_id}>
+              {mission.mission_name}
+            </li>
+          ))}
+        </ul>
+      </div>
+      <div>
+        <h1>My Rockets</h1>
+      </div>
+    </div>
+  );
+};
 
 export default Profile;
