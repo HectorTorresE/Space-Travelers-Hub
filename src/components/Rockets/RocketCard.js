62d1cc07bc02e2c29a51ca43e6e3874799036dd2
HectorTorresE <hectorjte@gmail.com>
2022-04-06 09:53:02 -0600

    Add css for displaying rockets

---
+++ src/components/Rockets/RocketCard.js
@@ -0,0 +1,27 @@
+import PropTypes from 'prop-types';
+import './Card.css';
+
+const RocketCard = ({
+  id, name, description, image,
+}) => (
+  <div className="rocket-card">
+    <img src={image} alt="rocket type" />
+    <div className="info-rocket">
+      <h3>
+        {name}
+      </h3>
+      <p>
+        {description}
+      </p>
+      <button type="button" data-id={id}>Reserve Rocket</button>
+    </div>
+  </div>
+);
+RocketCard.propTypes = {
+  id: PropTypes.number.isRequired,
+  name: PropTypes.string.isRequired,
+  description: PropTypes.string.isRequired,
+  image: PropTypes.string.isRequired,
+};
+
+export default RocketCard;
--- src/components/Rockets/Rockets.js
+++ src/components/Rockets/Rockets.js
@@ -1,21 +1,18 @@
 import { useSelector } from 'react-redux';
+import RocketList from './RocketList';
 
 const Rockets = () => {
   const rocketsList = useSelector((state) => state.rockets.rockets);
   const fetching = useSelector((state) => state.rockets.fetching);
   return (
-    <div>
+    <>
       {!fetching && (
-        <ul>
-          {rocketsList.map((rocket) => (
-            <li key={rocket.id}>{`id: ${rocket.id}, name: ${rocket.name}`}</li>
-          ))}
-        </ul>
+        <RocketList rockets={rocketsList} />
       )}
       {fetching && (
         <h3>Fetching data please wait...</h3>
       )}
-    </div>
+    </>
   );
 };
 
