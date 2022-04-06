ae770f43501ef386ee45d54d6c74eb6affe0396f
JuanLP <juan.luis.palacios.p@gmail.com>
2022-04-06 10:34:37 -0500

    Add on leave mission action

--- src/views/Missions.js
+++ src/views/Missions.js
@@ -2,7 +2,7 @@ import './Missions.css';
 import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import Mission from '../components/Mission';
-import { getMissions, joinMission } from '../redux/missions/missions';
+import { getMissions, joinMission, leaveMission } from '../redux/missions/missions';
 
 const Missions = () => {
   const dispatch = useDispatch();
@@ -30,6 +30,7 @@ const Missions = () => {
             description={mission.description}
             reserved={mission.reserved}
             onJoin={() => dispatch(joinMission(mission.mission_id))}
+            onLeave={() => dispatch(leaveMission(mission.mission_id))}
           />
         ))}
       </table>
